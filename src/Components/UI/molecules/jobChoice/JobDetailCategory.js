125b31c2c60b3713248f5cf4162e14b80149230d
seonjae choi <tjswo2292@naver.com>
2022-10-27 12:28:10 +0900

    jobDetail 카테고리 완성

--- src/Components/UI/molecules/jobChoice/JobDetailCategory.js
+++ src/Components/UI/molecules/jobChoice/JobDetailCategory.js
@@ -1,5 +1,69 @@
+import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
+
+import "./jobDetailCategory.css";
+
 const JobDetailCategory = () => {
-  return;
+  return (
+    <div className="job-category-wrap">
+      <div className="job-prev-btn">
+        <button>
+          <AiOutlineLeft className="left-btn" />
+        </button>
+      </div>
+      <ul className="job-category-container">
+        <li className="job-category-item">
+          <button>
+            <span>연봉이 최고의 복지 🎄</span>
+          </button>
+        </li>
+        <li className="job-category-item">
+          <button>
+            <span>재택근무 🏠</span>
+          </button>
+        </li>
+        <li className="job-category-item">
+          <button>
+            <span>퇴사율 10% 이하 📍</span>
+          </button>
+        </li>
+        <li className="job-category-item">
+          <button>
+            <span>급성장 중 📈</span>
+          </button>
+        </li>
+        <li className="job-category-item">
+          <button>
+            <span>병역특례 👦</span>
+          </button>
+        </li>
+        <li className="job-category-item">
+          <button>
+            <span>50인 이하 ⬇️</span>
+          </button>
+        </li>
+        <li className="job-category-item">
+          <button>
+            <span>50인 이상 ⬆️</span>
+          </button>
+        </li>
+        <li className="job-category-item">
+          <button>
+            <span>업력 5년 이상 🏢</span>
+          </button>
+        </li>
+        <li className="job-category-item">
+          <button>
+            <span>유연근무 🕚</span>
+          </button>
+        </li>
+      </ul>
+      <div className="job-more-container">
+        <button>
+          <AiOutlineRight className="right-btn" />
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default JobDetailCategory;
