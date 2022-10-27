ddd2106ee8e7dc412f4465fdbb21c9206aa594ac
seonjae choi <tjswo2292@naver.com>
2022-10-27 12:53:12 +0900

    기존 li 삭제

--- src/Components/UI/molecules/jobChoice/JobDetailCategory.js
+++ src/Components/UI/molecules/jobChoice/JobDetailCategory.js
@@ -27,51 +27,6 @@ const JobDetailCategory = () => {
             </button>
           </li>
         ))}
-        {/* <li className="job-category-item">
-          <button>
-            <span>연봉이 최고의 복지 🎄</span>
-          </button>
-        </li>
-        <li className="job-category-item">
-          <button>
-            <span>재택근무 🏠</span>
-          </button>
-        </li>
-        <li className="job-category-item">
-          <button>
-            <span>퇴사율 10% 이하 📍</span>
-          </button>
-        </li>
-        <li className="job-category-item">
-          <button>
-            <span>급성장 중 📈</span>
-          </button>
-        </li>
-        <li className="job-category-item">
-          <button>
-            <span>병역특례 👦</span>
-          </button>
-        </li>
-        <li className="job-category-item">
-          <button>
-            <span>50인 이하 ⬇️</span>
-          </button>
-        </li>
-        <li className="job-category-item">
-          <button>
-            <span>50인 이상 ⬆️</span>
-          </button>
-        </li>
-        <li className="job-category-item">
-          <button>
-            <span>업력 5년 이상 🏢</span>
-          </button>
-        </li>
-        <li className="job-category-item">
-          <button>
-            <span>유연근무 🕚</span>
-          </button>
-        </li> */}
       </ul>
       <div className="job-more-container">
         <button>
