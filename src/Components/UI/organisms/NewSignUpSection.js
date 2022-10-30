4f7c777ed792478ba272ef6e2aec9e09c4500350
seonjae choi <tjswo2292@naver.com>
2022-10-31 00:28:38 +0900

    회원가입 UI 수정

--- src/Components/UI/organisms/NewSignUpSection.js
+++ src/Components/UI/organisms/NewSignUpSection.js
@@ -1,15 +1,12 @@
-import NewSignUpTitle from "../molecules/newSignUp/NewSignUpTitle";
+// import NewSignUpTitle from "../molecules/newSignUp/NewSignUpTitle";
 import NewSignUpForm from "../molecules/newSignUp/NewSignUpForm";
 
 import "./newSignUpSection.css";
 
 const NewSignUpSection = () => {
   return (
-    <div className="new-sign-up-section-wrap">
-      <NewSignUpTitle />
-      <div className="new-sign-up-section">
-        <NewSignUpForm />
-      </div>
+    <div className="new-sign-up-section">
+      <NewSignUpForm />
     </div>
   );
 };
--- src/Components/pages/NewSignUpPage.js
+++ src/Components/pages/NewSignUpPage.js
@@ -1,5 +1,5 @@
 import NewSignUpTemp from "../templates/NewSignUpTemp";
-
+import "./newSignUpPage.css";
 const NewSignUpPage = () => {
   return (
     <div className="new-sign-up-page">
